f81724a037f87fba478d285fb4c7f43f7180cdb4
chuckrodgers <amorcine1@gmail.com>
2016-05-28 14:11:57 -0700

    added comment

--- MazeRunner/app/src/main/java/com/example/andrewgarcia/mazerunner/MazeActivity.java
+++ MazeRunner/app/src/main/java/com/example/andrewgarcia/mazerunner/MazeActivity.java
@@ -1,11 +1,8 @@
 package com.example.andrewgarcia.mazerunner;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -17,7 +14,7 @@ public class MazeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_maze);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        //
 
     }
 
