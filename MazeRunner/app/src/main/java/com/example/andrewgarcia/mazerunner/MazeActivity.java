ccb49e4e44d02cffb60560198cafa683eaf40481
Andrew Garcia <andrewgarcia@Andrews-MacBook-Air.local>
2016-06-07 19:12:15 -0700

    timer was added

--- MazeRunner/app/src/main/java/com/example/andrewgarcia/mazerunner/MazeActivity.java
+++ MazeRunner/app/src/main/java/com/example/andrewgarcia/mazerunner/MazeActivity.java
@@ -1,15 +1,19 @@
 package com.example.andrewgarcia.mazerunner;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class MazeActivity extends AppCompatActivity {
 
@@ -19,18 +23,99 @@ public class MazeActivity extends AppCompatActivity {
     private ImageView photo;
     int row, col;
 
+    // Button and Clock View
+    Button startButton, resetButton, solveButton;
+    TextView clockView;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maze);
 
-        RelativeLayout container = (RelativeLayout) findViewById(R.id.maze_container);
-        boardView = new MazeBoardView(this);
 
-        // Some setup of the view.
-        boardView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
-        container.addView(boardView);
+        startButton = (Button) findViewById(R.id.startButton);
+        resetButton = (Button) findViewById(R.id.resetButton);
+        solveButton = (Button) findViewById(R.id.solve_button);
+
+        clockView = (TextView) findViewById(R.id.clockView);
+
+        final clockClass timer = new clockClass(60000, 1000);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.start();
+                startButton.setEnabled(false);
+
+                Bitmap testMaze = BitmapFactory.decodeResource(getResources(),R.drawable.testmaze);
+
+
+                boardView.initialize(testMaze);
+                boardView.invalidate();
+
+
+
+            }
+        });
+
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //timer.cancel();
+                timer.start();
+                //startButton.setEnabled(true);
+            }
+        });
+
+
+
+
+
+
+
+
+       RelativeLayout container = (RelativeLayout) findViewById(R.id.maze_container);
+       boardView = new MazeBoardView(this);
+
+       // Some setup of the view.
+       boardView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
+       container.addView(boardView);
     }
+
+
+
+
+    public class clockClass extends CountDownTimer {
+
+        public clockClass(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            long millis = millisUntilFinished;
+            String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
+                    TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
+                    TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+            System.out.println(hms);
+            clockView.setText(hms);
+
+        }
+
+        @Override
+        public void onFinish() {
+            clockView.setText("Times Up.");
+        }
+    }
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -54,23 +139,23 @@ public class MazeActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            boardView.initialize(imageBitmap);
-            boardView.invalidate();
+    //@Override
+    //protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    //    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+    //        Bundle extras = data.getExtras();
+    //        Bitmap imageBitmap = (Bitmap) extras.get("data");
+    //        boardView.initialize(imageBitmap);
+     //       boardView.invalidate();
+//
+    //    }
+    //}
 
-        }
-    }
-
-    public void dispatchTakePictureIntent(View view) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
+    //public void dispatchTakePictureIntent(View view) {
+    //    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    //    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+    //        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    //    }
+    //}
 
     public void shuffleImage(View view) {
 
